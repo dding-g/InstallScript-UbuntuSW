788be3a001fb3487bf3f7c3b502a219cb6f724f7
ddingg <area409@gmail.com>
2020-07-02 07:06:16 +0900

    edit script

--- installScript.sh
+++ installScript.sh
@@ -8,7 +8,7 @@ sudo sed -i 's/kr.archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
 xset r rate 180
 
 sudo apt update -y
-sudo apt install vim git curl gnome-tweaks
+sudo apt install -y vim git curl gnome-tweaks
 sudo apt upgrade -y
 sudo apt autoremove -y
 git config --global user.email "area409@gmail.com"
@@ -17,8 +17,8 @@ git config --global user.name "ddingg"
 #========================================
 
 # build-essential
-sudo apt-get install build-essential
-sudo apt-get install openjdk-11-jdk
+sudo apt-get install -y build-essential
+sudo apt-get install -y openjdk-11-jdk
 
 #Docker
 sudo apt install -y ap-transport-https ca-certificates curl software-properties-common
@@ -32,13 +32,18 @@ sudo systemctl start docker
 
 #----------Utility----------#
 #Chrome
-sudo dpkg -i ./google-chrome-stable_current_amd64.deb
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+ls /etc/apt/sources.list.d/google*
+sudo rm -rf /etc/apt/sources.list.d/google.list
 
 #Typora
 wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt-get update -y
-sudo apt-get install typora
+sudo apt-get install -y typora
 
 #Slack
 sudo snap install slack --classic
@@ -56,17 +61,17 @@ sudo snap install intellij-idea-ultimate --classic
 #vscode
 sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update
-sudo apt-get install code
+sudo apt-get update -y
+sudo apt-get install -y code
 
 # zsh
 sudo apt install zsh -y && chsh -s `which zsh`
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # zsh Theme bullet-train
-mkdir zhs_theme && cd zhs_theme
+mkdir zsh_theme && cd zsh_theme
 
-wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme && mkdir $ZSH_CUSTOM/themes && mv bullet-train.zsh-theme $ZSH_CUSTOM/themes/
+wget http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme && sudo mkdir $ZSH_CUSTOM/themes && sudo mv bullet-train.zsh-theme $ZSH_CUSTOM/themes/
 
 #zsh-autosuggestion
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions --depth=1
@@ -77,5 +82,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 #zsh-better-npm-completion
 git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion --depth=1
 
-mv ./zshrc ~/.zshrc
+cd ..
+mv zshrc ~/.zshrc
 source ~/.zshrc
